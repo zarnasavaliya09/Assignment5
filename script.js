099bc7fcd26619014a8550d3457f9e0826ddc2b7
zarnasavaliya09 <113407633+zarnasavaliya09@users.noreply.github.com>
2023-03-02 21:50:01 -0500

    add border,width,padding,margin

--- script.js
+++ script.js
@@ -1,68 +1,60 @@
 /*
-    Assignment 05
+    Assignment 05 Zarna Savaliya
 */
 
 $(document).ready(function () {
-    // your code here
-    const contentItemList = $('#content-item-list');
+  // your code here
+  const contentItemList = $("#content-item-list");
 
-    items.forEach(function(item){
-        const contentItemHtml = `<div class="content-item-wrapper" id="contentitem-${item.id}">
+  items.forEach(function (item) {
+    const contentItemHtml = `<div class="content-item-wrapper" id="contentitem-${
+      item.id
+    }">
                                 ${item.toString()}
                              </div>`;
 
-        contentItemList.append(contentItemHtml);
-         $(`#contentitem-${item.id}`).css({
-                               'border': '1px solid black',
-                               'width': '80%',
-                               'padding': '10px',
-                               'margin': '20px auto'
-                             });
-
+    contentItemList.append(contentItemHtml);
+    $(`#contentitem-${item.id}`).css({
+      border: "2px solid gray",
+      width: "90%",
+      padding: "10px",
+      margin: "20px auto",
     });
-
-
-    
-
+  });
 });
-class ContentItem{
-    constructor(id,name,description,genre){
-        this.id=id;
-        this.name=name;
-        this.description=description;
-        this.genre=genre;
+class ContentItem {
+  constructor(id, name, description, genre) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.genre = genre;
+  }
+  updateContentItem(id, name, description, genre) {
+    if (this.id == id) {
+      if (name != null) {
+        this.name = name;
+      }
+      if (description != null) {
+        this.description = description;
+      }
+      if (genre != null) {
+        this.genre = genre;
+      }
     }
-    updateContentItem(id,name,description,genre){
-       if(this.id==id){
-            if(name!=null){
-                this.name=name;
-            }
-            if(description!=null){
-                this.description=description;
-            }
-            if(genre!=null){
-                this.genre=genre;
-            }
-            
-       }
-    }
-    toString(){
-        return `
+  }
+  toString() {
+    return `
                 <div class="content-item-wrapper" id="contentitem-${this.id}">
                 <h2>${this.name}</h2>
                 <p>${this.description}</p>
                 <div>${this.genre}</div> 
                 </div>`;
-       }  
-
-      
+  }
 }
 let items = [
-    new ContentItem(0, 'Lion', 'The king of the jungle', 'Big Cats'),
-    new ContentItem(1, 'Elephant', 'The largest land animal', 'Wildlife'),
-    new ContentItem(2, 'Giraffe', 'The tallest land animal', 'Wildlife'),
-    new ContentItem(3, 'Penguin', 'Flightless birds that live in Antarctica', 'Birds'),
-    new ContentItem(4, 'Octopus', 'Smart and elusive creatures that live in the ocean', 'Marine Life')
-  ];
-
-
+  new ContentItem(0, "The Shawshank Redemption", "The king of the jungle", "Drama"),
+  new ContentItem(1, "The Godfather", "reluctant youngest son", "Action"),
+  new ContentItem(2, "The Dark Knight", "psychological and physical tests", "Drama"),
+  new ContentItem(3,"Schindler's List","Flightless birds that live in Antarctica","Horror"),
+  new ContentItem(4,"Pulp Fiction","basic compassion","Comedy"),
+];
